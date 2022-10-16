5a26f1428d33af089ff9913ff15ece4a338458a7
eojin312 <eojin312@naver.com>
2022-10-16 22:15:13 +0900

    함수형 인터페이스 간단하게

--- src/main/java/test/openapi/java8/Foo.java
+++ src/main/java/test/openapi/java8/Foo.java
@@ -2,11 +2,6 @@ package test.openapi.java8;
 
 public class Foo {
     public static void main(String[] args) {
-        RunSomeThing runSomeThing = new RunSomeThing() {
-            @Override
-            public void doit() {
-                System.out.println("good");
-            }
-        };
+        RunSomeThing runSomeThing = () -> System.out.println("good");
     }
 }
