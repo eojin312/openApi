c22dddbace331d4cec3002b9db8152786d99baf5
eojin312 <eojin312@naver.com>
2022-10-16 23:40:05 +0900

    함수형 인터페이스의 여러가지.. 형태

--- src/main/java/test/openapi/java8/Foo.java
+++ src/main/java/test/openapi/java8/Foo.java
@@ -1,5 +1,14 @@
 package test.openapi.java8;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
+
+@Slf4j
 public class Foo {
     public static void main(String[] args) {
         RunSomeThing runSomeThing = (number) -> System.out.println("good" + number + 1);
@@ -8,5 +17,33 @@ public class Foo {
         runSomeThing.doit(3);
         runSomeThing.doit(4);
         runSomeThing.doit(5);
+
+        Plus plus = new Plus();
+
+        plus.apply(1);
+
+        Function<Integer, Integer> multiple = (integer) -> integer + 10;
+        Function<Integer, Integer> composeInteger = (integer) -> integer * 2;
+        Function<Integer, Integer> composeIntegerAndPlus10 = (integer) -> integer * 2;
+
+
+        log.info("[FUNCTION] andthen :{}", plus.andThen(composeInteger).toString());
+
+        Consumer<Integer> printT = (i) -> System.out.println(i);
+
+        printT.accept(10);
+
+        Supplier<Integer> get10 = () -> 10;
+
+        System.out.println(get10);
+
+        Predicate<String> startWithEojin = (s) -> s.startsWith("Eojin312");
+
+        System.out.println(startWithEojin);
+
+
+        UnaryOperator<Integer> plus10 = integer -> integer + 100;
+
+
     }
 }
---
+++ src/main/java/test/openapi/java8/Plus.java
@@ -0,0 +1,11 @@
+package test.openapi.java8;
+
+import java.util.function.Function;
+
+public class Plus implements Function<Integer, Integer> {
+
+    @Override
+    public Integer apply(Integer integer) {
+        return integer + 1;
+    }
+}
